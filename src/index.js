1f8d05da7ed3a4a3c852edd9660d94206a58cb82
Andrew J. Rasmussen <rasmussenandrew@outlook.com>
2024-07-22 22:28:32 +0200

    Minor adjustments.

--- src/index.js
+++ src/index.js
@@ -55,7 +55,7 @@ root.render(
           customClass='row-reverse'
         >
           <h2>2x 30 års fødselsdag</h2>
-          <p>Da vi begge er rundet de 30 inden for det sidste år, ønsker vi i samme anledning at fejre dette med jer.</p>
+          <p>Vi er begge rundet de 30 inden for det sidste år, og ønsker i samme anledning at fejre dette med jer.</p>
           <p>Der vil være <strong>mad og forplejning</strong> i løbet af dagen og der vil blive bestilt <strong>pizza til aftensmad</strong>, for dem der har lyst til at blive længere. Efter aftensmaden vil der være et <strong>underholdende indslag</strong>, som nok skal få gang i lattermusklerne.</p>
         </TextImgSection>
         <GallerySection
@@ -66,6 +66,7 @@ root.render(
     <ParallaxImgSection />
     <Footer>
       <h3>S.U. senest <strong>d. 1 oktober 2024</strong></h3>
+      <p>Meld også gerne tilbage om I bliver til aftensmad og hygge.</p>
       <p>Vi glæder os til at se jer!</p>
     </Footer>
   </React.StrictMode>
